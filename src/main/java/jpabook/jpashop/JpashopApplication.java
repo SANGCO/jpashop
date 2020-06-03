cd8e1a07932473ae6fe075c78bb70dae4a30f396
SANGCO <nsg1213@gmail.com>
2020-06-03 23:30:20 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		Hello h = new Hello();
+		h.setData("테스트");
+		String data = h.getData();
+		System.out.println(data);
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
